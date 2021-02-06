bd0c2ab22b603f0262f2aef4c60c95f75eddceed
Melih VARILCI <45106791+MelihVarilci@users.noreply.github.com>
2021-02-06 22:51:25 +0300

    Add files via upload

---
+++ Business/Concrete/CarManager.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Business.Abstract;
+using DataAccess.Abstract;
+using DataAccess.Concrete.InMemory;
+using Entities.Concrete;
+
+namespace Business.Concrete
+{
+    public class CarManager:ICarService
+    {
+        ICarDal _cars;
+
+        public CarManager(ICarDal cars)
+        {
+            _cars = cars;
+        }
+
+        public List<Car> GetById(int carId)
+        {
+            return _cars.GetById(carId);
+        }
+
+        public List<Car> GetAll()
+        {
+            return _cars.GetAll();
+        }
+
+        public void Add(Car car)
+        {
+            _cars.Add(car);
+        }
+
+        public void Update(Car car)
+        {
+            _cars.Update(car);
+        }
+
+        public void Delete(Car car)
+        {
+            _cars.Delete(car);
+        }
+    }
+}
